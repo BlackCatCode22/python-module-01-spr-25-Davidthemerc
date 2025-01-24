f579e0da4ad14acd29ea89bcaacfb753a019bb75
David Mercado <dmerc90@gmail.com>
2025-01-23 19:14:07 -0800

    Changed how the try/except code is handled so they are not wrapped around all code, following example from Worked Exercise 3.2

--- gross_pay.py
+++ gross_pay.py
@@ -1,33 +1,32 @@
 # Prompt the user for weekly hours worked and hourly rate
 weeklyHours = input("Weekly hours worked? ")
+hourlyRate = input("Hourly pay rate? ")
 
 try:
-    hourlyRate = input("Hourly pay rate? ")
 
-    try:
-
-        # Convert string values to float
-        weeklyHours = float(weeklyHours)
-        hourlyRate = float(hourlyRate)
-
-        # Calculate gross pay
-        grossPay = round(weeklyHours * hourlyRate,2)
-
-        # If user worked more than 40 hours in the week, calculate overtime pay
-        if weeklyHours > 40:
-            grossPay = round((hourlyRate * 40) + (weeklyHours - 40) * (hourlyRate * 1.5),2)
-            overPay = round((weeklyHours - 40) * (hourlyRate * 1.5),2)
-
-            # Print gross pay
-            print("Your gross pay is: $" + str(grossPay) + ".")
-            print("You were paid $" + str(overPay) + " of overtime pay.")
-
-        else:
-            # Print gross pay (no overtime)
-            print("Your gross pay is: $" + str(grossPay))
-
-    except:
-        print("Please enter numbers only.")
+    # Convert string values to float
+    weeklyHours = float(weeklyHours)
+    hourlyRate = float(hourlyRate)
 
 except:
-    print("Please enter numbers only.")
+    print("Please review the entered data.")
+    quit()
+
+# Calculate gross pay
+grossPay = round(weeklyHours * hourlyRate,2)
+
+# If user worked more than 40 hours in the week, calculate overtime pay
+if weeklyHours > 40:
+    grossPay = round((hourlyRate * 40) + (weeklyHours - 40) * (hourlyRate * 1.5),2)
+    overPay = round((weeklyHours - 40) * (hourlyRate * 1.5),2)
+
+    # Print gross pay
+    print("Your gross pay is: $" + str(grossPay) + ".")
+    print("You were paid $" + str(overPay) + " of overtime pay.")
+
+else:
+    # Print gross pay (no overtime)
+    print("Your gross pay is: $" + str(grossPay))
+
+
+
