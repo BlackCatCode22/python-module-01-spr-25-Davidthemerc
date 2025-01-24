e519cda5e37fc0fbeb540094418698b4bcb49476
David Mercado <dmerc90@gmail.com>
2025-01-23 19:05:09 -0800

    Reinstated separate overtime pay calculation so it can be displayed to the user

--- gross_pay.py
+++ gross_pay.py
@@ -16,9 +16,11 @@ try:
         # If user worked more than 40 hours in the week, calculate overtime pay
         if weeklyHours > 40:
             grossPay = round((hourlyRate * 40) + (weeklyHours - 40) * (hourlyRate * 1.5),2)
+            overPay = round((weeklyHours - 40) * (hourlyRate * 1.5),2)
 
             # Print gross pay
-            print("Your gross pay is: $" + str(grossPay) + ", including overtime.")
+            print("Your gross pay is: $" + str(grossPay) + ".")
+            print("You were paid $" + str(overPay) + " of overtime pay.")
 
         else:
             # Print gross pay (no overtime)
